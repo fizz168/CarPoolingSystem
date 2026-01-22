da5f9f8e728f693f2c71d025d674176efa383a4d
fizz168 <nheanomra@gmail.com>
2026-01-22 23:33:07 +0700

    feat : Refactor Customer class to improve method formatting and structure

--- src/Customer.java
+++ src/Customer.java
@@ -6,8 +6,14 @@ public Customer(String customerId, String name, int phoneNumber){
     this.customerId = customerId;
     this.name = name;
     this.phoneNumber = phoneNumber;
-
-
-
+}
+public String getcustomerId(){
+    return customerId;
+}
+public String getname(){
+    return name;
+}
+public int getphoneNumber(){
+    return phoneNumber;
 }
 }
