86fb01a3bc8aba9de739b1cfaad8076e9094896e
fizz168 <nheanomra@gmail.com>
2026-01-22 21:59:09 +0700

    Remove App class and add Car class with initial attributes and main method

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+class Car{
+    private String carId;
+    private String motoId;
+    private String brand;
+    private String model;
+    private double pricePerDay;
+    private Boolean isAvalible;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
