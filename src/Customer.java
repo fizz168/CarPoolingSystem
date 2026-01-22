0aa852bb37fd676b559b009f3a6ec7f9b3b1c1cc
fizz168 <nheanomra@gmail.com>
2026-01-22 23:28:45 +0700

    Add Customer class with attributes and constructor

---
+++ src/Customer.java
@@ -0,0 +1,13 @@
+ class Customer {
+    private String customerId;
+    private String name;
+    private int phoneNumber;
+public Customer(String customerId, String name, int phoneNumber){
+    this.customerId = customerId;
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+
+
+
+}
+}
