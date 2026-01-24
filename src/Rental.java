3d3c66a8d631a97b5e5466d1c4f3ef47ae9bc963
fizz168 <nheanomra@gmail.com>
2026-01-24 10:47:51 +0700

    feat: Enhance Rental class with getter methods for attributes

--- src/Rental.java
+++ src/Rental.java
@@ -4,11 +4,22 @@
     private Customer customer;
     private int days;
 
-    public Rental(Vehicle car, Vehicle moto, Customer customer, int days ){
+public Rental(Vehicle car, Vehicle moto, Customer customer, int days ){
         this.car = car;
         this.moto = moto;
         this.customer = customer;
         this.days = days;
-
     }
+public Vehicle getcar(){
+    return car;
+}
+public Vehicle getmoto(){
+    return moto;
+}
+public Customer getcustomer(){
+    return customer;
+}
+public int getdays(){
+    return days;
+}
 }
