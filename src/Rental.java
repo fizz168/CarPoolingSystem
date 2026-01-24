70973cb9a79927764c2c750db16b8577f91e8649
fizz168 <nheanomra@gmail.com>
2026-01-24 10:41:56 +0700

    feat: Add Rental class with attributes and constructor

---
+++ src/Rental.java
@@ -0,0 +1,14 @@
+ class Rental {
+    private Vehicle car;
+    private Vehicle moto;
+    private Customer customer;
+    private int days;
+
+    public Rental(Vehicle car, Vehicle moto, Customer customer, int days ){
+        this.car = car;
+        this.moto = moto;
+        this.customer = customer;
+        this.days = days;
+
+    }
+}
