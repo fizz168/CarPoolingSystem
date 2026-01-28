fb5ce25b3fc5ef2b58a6376fdebf8c738a530f90
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-01-28 14:19:14 +0700

    Fix : class

--- src/Customer.java
+++ src/Customer.java
@@ -1,7 +1,7 @@
-class Customer {
-    private String customerId;
-    private String name;
-    private int phoneNumber;
+public class Customer {
+     String customerId;
+     String name;
+     int phoneNumber;
 public Customer(String customerId, String name, int phoneNumber){
     this.customerId = customerId;
     this.name = name;
