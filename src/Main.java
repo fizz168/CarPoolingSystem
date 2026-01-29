8a49f79b3c4d6058879231d556dfa03b5e4bbd97
fizz168 <nheanomra@gmail.com>
2026-01-29 08:49:53 +0700

    fix: update Main class to include additional vehicle details in output

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Vehicle v = new Vehicle("M12", "T", "XL", 2.5, "moto");
         System.out.println(v.getbrand());
+        System.out.println(v.getvehicleINumberplate());
+        System.out.println(v.getvehicletype());
+        System.out.println(v.getmodel());
     }
     
 }
