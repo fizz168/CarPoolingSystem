4b1ac3a33d318508dc570c3b5bf9321ffa70c7a0
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-03 13:09:58 +0700

    fix : rental

--- src/Rental.java
+++ src/Rental.java
@@ -1,29 +1,14 @@
 class Rental {
-   private Vehicle vehicle;
-   private Customer customer;
-   private int days;
-   private int recordId;
 
-   public Rental(Vehicle vehicle, Customer customer, int days, int recordId) {
-      this.vehicle = vehicle;
-      this.customer = customer;
-      this.days = days;
-      this.recordId = recordId;
-   }
+    Vehicle vehicle;
+    Customer customer;
+    int days;
+    int recordId;
 
-   public Vehicle getvehicle() {
-      return this.vehicle;
-   }
-
-   public Customer getcustomer() {
-      return this.customer;
-   }
-
-   public int getdays() {
-      return this.days;
-   }
-   public int getrecordid(){
-      return recordId;
-   }
-  
-   }
+    Rental(Vehicle vehicle, Customer customer, int days, int recordId) {
+        this.vehicle = vehicle;
+        this.customer = customer;
+        this.days = days;
+        this.recordId = recordId;
+    }
+}
