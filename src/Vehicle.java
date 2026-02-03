1f9cf8bf43c3a4a52b65a72da82e2b3ea7a7c290
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-03 13:08:01 +0700

    fix : vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,42 +1,34 @@
-class Vehicle{
-    private String vehicleNumberPlate;
-    private String brand;
-    private String model;
-    private double pricePerDay;
-    private boolean isAvalible;
-    private String vehicleType;
-    
-public Vehicle (String vehicleNumberPlate, String brand, String model, double pricePerDay, String vehicleType){
-    this.vehicleNumberPlate = vehicleNumberPlate;
-    this.brand = brand;
-    this.model = model;
-    this.pricePerDay = pricePerDay;
-    this.isAvalible = true;
-    this.vehicleType = vehicleType;
-}
-public String getvehicleINumberplate(){
-    return vehicleNumberPlate;
-}
-public String getbrand(){
-    return brand;
-}
-public String getmodel(){
-    return model;
-}
-public double calulatePrice(int rentalDay){
-    return pricePerDay* rentalDay;
-}
-public boolean isAvalible(){
-    return isAvalible;
-}
-public void rent(){
-  isAvalible = false;
-}
-public void returnCar(){
-    isAvalible = true;
-}
-public String getvehicleType(){
-    return vehicleType;
-    
-}
+class Vehicle {
+
+    String vehicleNumberPlate;
+    String brand;
+    String model;
+    double pricePerDay;
+    boolean isAvalible;
+    String vehicleType;
+
+    Vehicle(String vehicleNumberPlate, String brand, String model, double pricePerDay, String vehicleType) {
+        this.vehicleNumberPlate = vehicleNumberPlate;
+        this.brand = brand;
+        this.model = model;
+        this.pricePerDay = pricePerDay;
+        this.isAvalible = true;
+        this.vehicleType = vehicleType;
+    }
+
+    double calulatePrice(int rentalDay) {
+        return pricePerDay * rentalDay;
+    }
+
+    boolean isAvalible() {
+        return isAvalible;
+    }
+
+    void rent() {
+        isAvalible = false;
+    }
+
+    void returnCar() {
+        isAvalible = true;
+    }
 }
