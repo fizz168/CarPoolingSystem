7a71022b393a0f59ba019fb478149eac6718f881
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-03 13:10:59 +0700

    fix : Main

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,26 @@
     
 // }
 
-public class Main {
+class Main {
     public static void main(String[] args) {
+
         Vehicle v = new Vehicle("M12", "Toyota", "Hilux", 50.0, "Car");
         Customer c = new Customer("C001", "John Doe", "012345678", "male");
         Staff s = new Staff("Alice", "S99", 10101, "098765432");
 
-        
         if (v.isAvalible()) {
-            v.rent(); 
-            Rental r = new Rental(v, c, 3, 5001); 
+            v.rent();
+            Rental r = new Rental(v, c, 3, 5001);
+
             System.out.println("--- Rental Receipt ---");
-            System.out.println("Customer: " + c.getname());
-            System.out.println("Phone number :" + c.getphoneNumber());
-            System.out.println("Vehicle: " + v.getvehicleINumberplate());
-            System.out.println("Total Cost: $" + v.calulatePrice(r.getdays()));
-          
-            System.out.println("Total Days :" + r.getdays());
+            System.out.println("Customer: " + c.name);
+            System.out.println("Phone number :" + c.phoneNumber);
+            System.out.println("Vehicle: " + v.vehicleNumberPlate);
+            System.out.println("Total Cost: $" + v.calulatePrice(r.days));
+            System.out.println("Total Days :" + r.days);
             System.out.println("Status: Vehicle is now rented.");
-            System.out.println("Staff info :" + s.getnameStaff() );
+            System.out.println("Staff info :" + s.nameStaff);
+
         } else {
             System.out.println("Sorry, this vehicle is not available.");
         }
