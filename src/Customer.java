9c204ff9cfb571644eeadfb75d57e80abfbc3883
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-03 13:11:40 +0700

    Fix : customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,23 +1,14 @@
 class Customer {
-    private String customerPersonalId;
-    private String name;
-    private String phoneNumber;
-    private String gender;
-public Customer(String customerPersonalId, String name, String phoneNumber, String gender){
-    this.customerPersonalId = customerPersonalId;
-    this.name = name;
-    this.phoneNumber = phoneNumber;
-}
-public String getcustomerPersonalId(){
-    return customerPersonalId;
-}
-public String getname(){
-    return name;
-}
-public String getphoneNumber(){
-    return phoneNumber;
-}
-public String getgender(){
-    return gender;
-}
+
+    String customerPersonalId;
+    String name;
+    String phoneNumber;
+    String gender;
+
+    Customer(String customerPersonalId, String name, String phoneNumber, String gender) {
+        this.customerPersonalId = customerPersonalId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;   // necessary fix
+    }
 }
