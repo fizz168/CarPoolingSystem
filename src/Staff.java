cb3af1156e779d1259804294a292ec9788df557c
Vinchaylim <vinchay.lim@student.cadt.edu.kh>
2026-02-03 13:09:15 +0700

    fix : staff

--- src/Staff.java
+++ src/Staff.java
@@ -1,26 +1,14 @@
-public class Staff {
+class Staff {
+
     String nameStaff;
     String staffId;
     int staffPersonalId;
     String staffPhoneNumber;
-public Staff(String nameStaff, String staffId, int staffPersonalId, String staffPhoneNumber){
-    this.nameStaff = nameStaff;
-    this.staffId = staffId;
-    this.staffPersonalId = staffPersonalId;
-    this.staffPhoneNumber = staffPhoneNumber;
 
-}
-public String getnameStaff(){
-    return nameStaff;
-}
-public String getstaffId(){
-    return staffId;
-}
-  public int getstaffPersonalId(){
-    return staffPersonalId;
-  }  
-public String getstaffPhoneNumber(){
-    return staffPhoneNumber;
-}
-
+    Staff(String nameStaff, String staffId, int staffPersonalId, String staffPhoneNumber) {
+        this.nameStaff = nameStaff;
+        this.staffId = staffId;
+        this.staffPersonalId = staffPersonalId;
+        this.staffPhoneNumber = staffPhoneNumber;
+    }
 }
