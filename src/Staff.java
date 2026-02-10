026a64be7e81501329404f5c5f7052b48650f579
fizz168 <nheanomra@gmail.com>
2026-02-10 21:02:01 +0700

    staff

--- src/Staff.java
+++ src/Staff.java
@@ -1,14 +1,25 @@
- class Staff {
+ public class Staff {
+    private String nameStaff;
+    private String staffId;
+    private int staffPersonalId;
+    private String staffPhoneNumber;
 
-    String nameStaff;
-    String staffId;
-    int staffPersonalId;
-    String staffPhoneNumber;
-
-  Staff(String nameStaff, String staffId, int staffPersonalId, String staffPhoneNumber) {
+public Staff(String nameStaff, String staffId, int staffPersonalId, String staffPhoneNumber) {
         this.nameStaff = nameStaff;
         this.staffId = staffId;
         this.staffPersonalId = staffPersonalId;
         this.staffPhoneNumber = staffPhoneNumber;
     }
+public String getNameStaff(){
+    return nameStaff;
+}
+public String getStaffId(){
+    return staffId;
+}
+public int getStaffPersonalId(){
+    return staffPersonalId;
+}
+public String getStaffPhoneNumber(){
+    return staffPhoneNumber;
+}
 }
