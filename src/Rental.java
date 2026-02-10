815fb745792c29f937ee465d1e5bf14ae72fe990
fizz168 <nheanomra@gmail.com>
2026-02-10 20:53:01 +0700

    feat : rental

--- src/Rental.java
+++ src/Rental.java
@@ -1,16 +1,31 @@
- class Rental {
+ public class Rental {
 
-    Vehicle vehicle;
-    Customer customer;
-    int days;
-    int recordId;
-    double priceAtBooking;
+    private Vehicle vehicle;
+    private Customer customer;
+    private int days;
+    private int recordId;
+    private double priceAtBooking;
 
-  Rental(Vehicle vehicle, Customer customer, int days, int recordId) {
+public Rental(Vehicle vehicle, Customer customer, int days, int recordId) {
         this.vehicle = vehicle;
         this.customer = customer;
         this.days = days;
         this.recordId = recordId;
         this.priceAtBooking = vehicle.getPricePerDay();
     }
+public Vehicle getVehicle(){
+    return vehicle;
+}
+public Customer getCustomer(){
+    return customer;
+}
+public int getDays(){
+    return days;
+}
+public int getRecordId(){
+    return recordId;
+}
+public double getPriceAtBooking(){
+    return priceAtBooking;
+}
 }
