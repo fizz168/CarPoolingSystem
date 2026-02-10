02c24e100bb4c14d2e5928cdea3d16c4e2326b8b
fizz168 <nheanomra@gmail.com>
2026-02-10 22:35:12 +0700

    add taxRate on garage

--- src/Garage.java
+++ src/Garage.java
@@ -2,10 +2,15 @@ public class Garage {
     private String garageName;
     private Vehicle[] inventory;
     private int vehicleCount;
+    private static double taxRate = 0.10;
 public Garage(String garageName, int capacity){
     this.garageName = garageName;
     this.inventory = new Vehicle[capacity];
     this.vehicleCount = 0;
+    taxRate ++;
+}
+public static double getTaxRate(){
+    return taxRate;
 }
 public String getGarageName(){
     return garageName;
